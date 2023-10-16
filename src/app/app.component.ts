09bae32f29b5691c46af44c02aa5ac0b704296ef
Josue Morales <molinajosue92@hotmail.com>
2023-10-16 00:14:29 -0400

    add home component and google sso button

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: "app-root",
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'family-cloud';
+	username = "";
+	password = "";
+	title = "family-cloud";
+
+	constructor(private http: HttpClient) {}
+
+	login(username: string, password: string) {
+		this.http.post("http://localhost:3000/login", {}, {}).subscribe((res) => {
+			console.log(res);
+		});
+	}
 }
